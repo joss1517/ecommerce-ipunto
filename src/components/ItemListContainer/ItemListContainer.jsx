a6d43efa61a94329aaf5219bd2d5f7084d1a4cca
josué pérez <ipunto09@gmail.com>
2024-06-08 18:19:35 -0600

    arreglo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import "./ItemListContainer.css";
 
-const ItemListContainer = ({saludo, saludo2}) => {
+const ItemListContainer = ({saludo, }) => {
   return (
       <div className="itemList">
           <h1 className="saludo">{ saludo }</h1>
